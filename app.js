4d47625bc9678ce37c31ebe75dbba8134b93c89a
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2022-11-08 17:40:39 +0600

    add scherma

--- app.js
+++ app.js
@@ -7,10 +7,53 @@ const mongoose = require("mongoose");
 app.use(express.json());
 app.use(cors());
 
+// Schema Design
+const productSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please Provide a Name for this Product."],
+        trim: true,
+        unique: [true, "Name must be unique."],
+        minlength: [3, "Name Must be at least 3 Charecters."],
+        maxlength: [100, "Name is too large."],
 
+    },
+    description: {
+        type: String,
+        required: [true, "Please Provide a Description for this Product."],
+    },
+    price: {
+        type: Number,
+        required: [true, "Please Provide a Price for this Product."],
+        min: [0, "Price Can't be Negative."],
+    },
+    unit: {
+        type: String,
+        required: [true],
+        enum: {
+            value: ["kg", "litre", "pcs"],
+            message: "Unit value Can't be {VALUE}, must be kg/litre/pcs."
+        },
+    },
+    quantity: {
+        type: Number,
+        required: true,
+        min: [0, "Quantity Can't be Negative."],
+        validate: {
+            validator: (value) => {
+                const isInteger = Number.isInteger(value);
+                if (isInteger) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+})
 
 app.get("/", (req, res) => {
-  res.send("Route is working! YaY!");
+    res.send("Route is working! YaY!");
 });
 
 
